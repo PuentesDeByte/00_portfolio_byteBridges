b7fe827962187c0479a518ae037922fa43d82d4a
nicolevenachi <ludwing@unicauca.edu.co>
2023-07-28 13:07:34 -0500

    navbar barely finished

--- src/assets/icons/Ham.jsx
+++ src/assets/icons/Ham.jsx
@@ -1,9 +1,9 @@
 
 const Ham = () => (
-  <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect className="top" x="20" y="29" width="60" height="7.85106" fill="#000" />
-    <rect className="middle" x="30" y="45.5745" width="50" height="7.85106" fill="#000" />
-    <rect className="bottom" x="40" y="62.1489" width="40" height="7.85106" fill="#000" />
+  <svg width="auto" height="auto" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect className="top" x="20" y="29" width="60" height="4.85106" fill="#4F96CC" rx="2" ry="2" />
+    <rect className="middle" x="30" y="45.5745" width="50" height="4.85106" fill="#4F96CC" rx="2" ry="2" />
+    <rect className="bottom" x="40" y="62.1489" width="40" height="4.85106" fill="#4F96CC" rx="2" ry="2" />
   </svg>
 )
 
