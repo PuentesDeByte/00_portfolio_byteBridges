31abe3490a9f249f0ddb7676ce4495998503bfc3
nicolevenachi <ludwing@unicauca.edu.co>
2023-07-27 19:40:37 -0500

    basic setup. File tree structured, and fonts imported

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './routes/App.jsx'
+import "./styles/_globales.scss"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
---
+++ src/routes/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      <h1>Con los nuevos textos</h1>
+    </>
+  )
+}
+
+export default App
