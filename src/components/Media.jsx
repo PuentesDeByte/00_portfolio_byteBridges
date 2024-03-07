94423b5be0f6ca8cfe672919e04607aff21c2fc0
nicolevenachi <ludwing@unicauca.edu.co>
2024-03-07 18:40:22 -0500

    media logos updated

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,4 +1,4 @@
-import IconGitHub from "../assets/icons/github"
+import IconUpwork from "../assets/icons/upwork"
 import IconLinkedin from "../assets/icons/linkedin"
 
 const Media = () => {
@@ -6,13 +6,13 @@ const Media = () => {
   return (
     <ul className="media-list w-full h-full flex justify-between m-0">
       <li className="w-auto h-full pr-5">
-        <a href="https://www.linkedin.com/in/nicole-venachi/" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/puentes-de-bytes-5924512b8/" target="_blank" rel="noreferrer">
           <IconLinkedin />
         </a>
       </li>
       <li className="w-auto h-full pl-5">
-        <a href="https://github.com/NicoleVenachi" target="_blank" rel="noreferrer">
-          <IconGitHub />
+        <a href="https://www.upwork.com/freelancers/~010eea89cf3dc3f0d7" target="_blank" rel="noreferrer">
+          <IconUpwork />
         </a>
       </li>
     </ul>
